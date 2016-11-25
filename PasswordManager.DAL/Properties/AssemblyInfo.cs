d5943e4c52b3a8aa7085dfb1889d043d015d693f
Sajjad Gul <sajjadarifgul@gmail.com>
2016-11-25 17:46:36 +0500

    Data Access Layer

--- PasswordManager.DAL/Properties/AssemblyInfo.cs
+++ PasswordManager.DAL/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PasswordManager.DLL")]
+[assembly: AssemblyTitle("PasswordManager.DAL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PasswordManager.DLL")]
+[assembly: AssemblyProduct("PasswordManager.DAL")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d68928a0-8e7b-4bb7-8a51-06a45fe376bd")]
+[assembly: Guid("8e8dbc25-61e7-48cd-8648-bca2765d2d57")]
 
 // Version information for an assembly consists of the following four values:
 //
