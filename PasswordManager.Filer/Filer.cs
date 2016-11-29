e41e647076341dc7337d464e739a8b93ab3301fc
Sajjad Gul <sajjadarifgul@gmail.com>
2016-11-29 10:56:50 +0500

    Gulipso Encryption

---
+++ PasswordManager.Filer/Filer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Filer
+{
+    public class Filer
+    {
+        public bool Save(string Data, string FileName)
+        {
+
+
+            return false;
+        }
+
+        public string Read(string FileName)
+        {
+
+
+            return "";
+        }
+    }
+}
